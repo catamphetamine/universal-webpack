0d116496b8c72d8a7d6694fad0f06907d45a1f8d
Gert Hengeveld <info@ghengeveld.nl>
2017-01-28 21:09:47 +0100

    Ensure webpack-chunks.json is properly resolved.

--- source/chunks.js
+++ source/chunks.js
@@ -2,5 +2,9 @@ import path from 'path'
 
 export function chunk_info_file_path(webpack_configuration, chunk_info_filename)
 {
-	return path.join(webpack_configuration.output.path, chunk_info_filename || 'webpack-chunks.json')
+	return path.resolve(
+		webpack_configuration.context || process.cwd(),
+		webpack_configuration.output.path,
+		chunk_info_filename || 'webpack-chunks.json'
+	)
 }
